c04136ceec89cd61419ef864a481ab8c3af68caf
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-02 22:03:44 +0530

    Delete Day10and11 directory

--- Day10and11/Takingthreeinput.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter Number"
-read A B C
-res[A+B*C]=$((A+B*C))
-res[C+A/B]=$((C+A/B))
-res[A*B+C]=$((A*B+C))
-res[A%B+C]=$((A%B+C))
-echo ${res[*]}
-IFS=$'\n' SORTED=($(sort <<<"${res[*]}"))
-printf '%s\n' "${res[@]}" | tac | tr '\n' ' ';
